a7c332d3ac58731d08d6985caab184d848070487
KillianDB <killiandomingues57@gmail.com>
2023-04-28 11:10:45 -0300

    DesafioI resolvido

---
+++ src/desafios/DesafioI/components/index.jsx
@@ -0,0 +1,43 @@
+function Cientistas() {
+    return (
+        <div>
+        <h1>Cientistas Notáveis</h1>
+        <section>
+          <h2>Maria Sklodowska-Curie</h2>
+          <ul>
+            <li>
+              <b>Profissão: </b> 
+              física e química
+            </li>
+            <li>
+              <b>Prêmios: </b> 
+              (Prêmio Nobel de Física, Prêmio Nobel de Química, Medalha Davy, Medalha Matteucci)
+            </li>
+            <li>
+              <b>Descobriu: </b>
+              polônio (elemento)
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>Katsuko Saruhashi</h2>
+          <ul>
+            <li>
+              <b>Profissão: </b> 
+              geoquímica
+            </li>
+            <li>
+              <b>Prêmios: </b> 
+              (Prêmio Miyake de geoquímica, Prêmio Tanaka)
+            </li>
+            <li>
+              <b>Descobriu: </b>
+              um método para medir o dióxido de carbono na água do mar
+            </li>
+          </ul>
+        </section>
+      </div>       
+    );
+}
+
+export default Cientistas;
--- src/desafios/DesafioI/index.jsx
+++ src/desafios/DesafioI/index.jsx
@@ -1,44 +1,9 @@
 // Existe alguma estrutura sendo repetida no componente Galeria? Transforme em outro componente e escolha quais props passar para ele
+import Cientistas from "./components";
 
 function Galeria() {
     return (
-        <div>
-        <h1>Cientistas Notáveis</h1>
-        <section>
-          <h2>Maria Sklodowska-Curie</h2>
-          <ul>
-            <li>
-              <b>Profissão: </b> 
-              física e química
-            </li>
-            <li>
-              <b>Prêmios: </b> 
-              (Prêmio Nobel de Física, Prêmio Nobel de Química, Medalha Davy, Medalha Matteucci)
-            </li>
-            <li>
-              <b>Descobriu: </b>
-              polônio (elemento)
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Katsuko Saruhashi</h2>
-          <ul>
-            <li>
-              <b>Profissão: </b> 
-              geoquímica
-            </li>
-            <li>
-              <b>Prêmios: </b> 
-              (Prêmio Miyake de geoquímica, Prêmio Tanaka)
-            </li>
-            <li>
-              <b>Descobriu: </b>
-              um método para medir o dióxido de carbono na água do mar
-            </li>
-          </ul>
-        </section>
-      </div>
+      <Cientistas/>
     );
   }
 
