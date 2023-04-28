a99bbb9a38be016237f620296a224e22d4e78da1
KillianDB <killiandomingues57@gmail.com>
2023-04-28 10:37:59 -0300

    DesafioD resolvido

---
+++ src/desafios/DesafioD/components/index.jsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+    return (
+        <section>
+            <h2>Criando componentes</h2>
+            <img src= '../../img/photo-1644794472051-36d154dfe487.avif' ></img>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit temporibus dolorum id veniam iure et soluta, exercitationem porro facere est deleniti totam libero possimus ipsa, nam ipsam fugiat aperiam laborum.</p>
+        </section>        
+    );
+}
+
+export default HelloWorld;
--- src/desafios/DesafioD/index.jsx
+++ src/desafios/DesafioD/index.jsx
@@ -2,3 +2,15 @@
 // - um título <h2> com o texto: Criando componentes
 // - a imagem com o atributo src para: https://images.unsplash.com/photo-1644794472051-36d154dfe487?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80
 // - um parágrafo com um texto qualquer 
+
+import HelloWord from './components';
+
+function Ola() {
+    return (
+      <main>
+        <HelloWord />
+      </main>
+    );
+}
+
+export default Ola;  
