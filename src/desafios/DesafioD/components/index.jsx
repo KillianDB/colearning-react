523d2431b4f797f0ada73ae8eac5cb4c00908a6c
KillianDB <killiandomingues57@gmail.com>
2023-04-28 10:40:43 -0300

    DesafioD resolvido(correção)

--- src/desafios/DesafioD/components/index.jsx
+++ src/desafios/DesafioD/components/index.jsx
@@ -2,7 +2,7 @@ function HelloWorld() {
     return (
         <section>
             <h2>Criando componentes</h2>
-            <img src= '../../img/photo-1644794472051-36d154dfe487.avif' ></img>
+            <img src= 'https://images.unsplash.com/photo-1644794472051-36d154dfe487?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80' ></img>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit temporibus dolorum id veniam iure et soluta, exercitationem porro facere est deleniti totam libero possimus ipsa, nam ipsam fugiat aperiam laborum.</p>
         </section>        
     );
