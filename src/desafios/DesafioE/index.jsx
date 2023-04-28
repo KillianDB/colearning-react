36d459b43d61abed68fafc501bee4a88705c5e9b
KillianDB <killiandomingues57@gmail.com>
2023-04-28 10:43:27 -0300

    DesafioE resolvido

--- src/desafios/DesafioE/index.jsx
+++ src/desafios/DesafioE/index.jsx
@@ -1,2 +1,13 @@
-// Copie e colea estrutura do componente HelloWorld (desafio D), e altere o nome do componente para Hello.
+// Copie e cole a estrutura do componente HelloWorld (desafio D), e altere o nome do componente para Hello.
 // Modifique a maneira como a imagem é exibida. Agora, vocêe deverá baixar a imagem para o projeto (pode colocá-la na pasta do Desafio E, se preferir), e faça a importação dela para usá-la em seu componente.
+import Hello from './components';
+
+function OlaDiferenciado() {
+    return (
+      <main>
+        <Hello />
+      </main>
+    );
+}
+
+export default OlaDiferenciado; 
