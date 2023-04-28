0eb4c6b3fd178e20d55b4712c13ad24f62066e6f
KillianDB <killiandomingues57@gmail.com>
2023-04-28 10:47:50 -0300

    DesafioF resolvido

--- src/desafios/DesafioF/index.jsx
+++ src/desafios/DesafioF/index.jsx
@@ -1,12 +1,14 @@
-// function MinhaLista() {
-//     return (
-//         <h1>A fazer</h1>
-//         <ol>
-//           <li>Preparar malas para viagem
-//           <li>Abastecer carro
-//           <li>Regar plantas
-//         </ol>
-//     );
-// }
+function MinhaLista() {
+     return (
+        <main>
+         <h1>A fazer</h1>
+         <ol>
+           <li>Preparar malas para viagem</li>
+           <li>Abastecer carro</li>
+           <li>Regar plantas</li>
+           </ol>
+         </main>
+     );
+ }
 
-// export default MinhaLista;
+ export default MinhaLista;
